2e19e205e6fc431126deaf4891117a72a91d4b35
Abubakar Lawan <abubakarlawan671@gmail.com>
2025-01-27 13:07:29 +0100

    Mkae deliveryOption Interactive

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import { cart, removeFromCart, calculateCartQuantity, updateQuantity } from "../data/cart.js";
+import { cart, removeFromCart, calculateCartQuantity, updateQuantity, updateDeliveryOption } from "../data/cart.js";
 import { products } from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
 import { hello } from "https://unpkg.com/supersimpledev@1.0.1/hello.esm.js";
@@ -116,9 +116,11 @@ let html = '';
     const isChecked = deliveryOption.id === cartItem.deliveryOptionId;
 
    html += `
-      <div class="delivery-option">
+      <div class="delivery-option js-delivery-option"
+      data-product-id="${matchingProduct.id}"
+      data-delivery-option-id="${deliveryOption.id}">
     <input type="radio"
-    ${isChecked ? 'checked' : ''}
+    ${isChecked ? "checked" : ""}
       class="delivery-option-input"
       name="delivery-option-${matchingProduct.id}">
     <div>
@@ -176,4 +178,12 @@ document.querySelector(".js-order-summary")
         updateCartQuantity();
       });
     });
+
+    document.querySelectorAll('.js-delivery-option')
+     .forEach((element) => {
+      element.addEventListener('click', () => {
+        const {productId, deliveryOptionId} = element.dataset;
+        updateDeliveryOption(productId, deliveryOptionId);
+      });
+     });
    
